6665f506255ded8cdf5be50eb7885f71b28bc6bc
emdad <emdad@ergo-ventures.com>
2023-04-11 15:34:56 +0600

    initial setup with dagger-hilt

---
+++ app/src/main/java/com/example/disher/categories/CategoriesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.disher.categories
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CategoriesScreen() {
+    Text(text = "Hello")
+}
