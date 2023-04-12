241ab2fe3563951e6ad08bf8d2d9c9329d01103f
emdad <emdad@ergo-ventures.com>
2023-04-12 21:44:04 +0600

    lazy column, card, row, category list screen design

--- app/src/main/java/com/example/disher/categories/CategoriesScreen.kt
+++ app/src/main/java/com/example/disher/categories/CategoriesScreen.kt
@@ -1,36 +1,68 @@
 package com.example.disher.categories
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
+import com.example.disher.categories.model.Category
 import com.example.disher.categories.viewmodel.CategoriesViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
 @Composable
 fun CategoriesScreen(
     categoriesViewModel: CategoriesViewModel = hiltViewModel()
 ) {
-    val categories = getFakeData()
-    Column {
-        Text(text = "INITIAL SETUP DONE")
-        Spacer(modifier = Modifier.height(30.dp))
-        categories.forEach {
-            Text(text = it)
+    //compose screen recompose redraws screen
+    // to survive that and also update new item
+    val listOfCategories by remember { categoriesViewModel.categoriesList }
+
+    LazyColumn {
+        items(listOfCategories) { item ->
+            SingleItemCategory(item)
         }
     }
+
 }
 
+@Composable
+fun SingleItemCategory(item: Category) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        elevation = 8.dp
+    ) {
 
-fun getFakeData(): List<String> {
-    return listOf(
-        "fish",
-        "vegan",
-        "meat"
-    )
+        Row(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+
+
+        ) {
+
+            Image(
+                modifier = Modifier.size(80.dp),
+                painter = rememberImagePainter(item.strCategoryThumb),
+                contentDescription = null
+            )
+            Text(text = item.strCategory, fontSize = 24.sp)
+
+        }
+    }
+
 }
+
--- app/src/main/java/com/example/disher/categories/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/com/example/disher/categories/viewmodel/CategoriesViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.disher.categories.viewmodel
 
 import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.disher.categories.model.Category
 import com.example.disher.categories.usecase.ICategoriesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,13 +16,19 @@ import javax.inject.Inject
 class CategoriesViewModel @Inject constructor(
     useCase: ICategoriesUseCase
 ) : ViewModel() {
+    //    state is a wrapper around data which emits changes to its listeners
+
+    //private one is mutable and receives changes from usecase
+    private val _categoriesList: MutableState<List<Category>> = mutableStateOf(emptyList())
+
+    // public one is immutable which is exposed to view (compose)
+    val categoriesList: State<List<Category>> = _categoriesList
+
     init {
         viewModelScope.launch {
             val categoriesResponse = useCase()
-            categoriesResponse.categories.forEach {
-                Log.d("CategoriesViewModel", it.strCategory)
-                Log.d("CategoriesViewModel", it.strCategoryDescription)
-            }
+            _categoriesList.value = categoriesResponse.categories
+
         }
     }
 }
